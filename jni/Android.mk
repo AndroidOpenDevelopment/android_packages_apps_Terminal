3c277e0105ca5411bbc27a9307f824307d7771b2
dhacker29 <dhackerdvm@gmail.com>
2014-12-22 02:26:51 -0500

    Revert "Update and fix Terminal."
    This reverts commit ffbad51da16422bab578c6616a714ad05ae20d73.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     jni_init.cpp \
     com_android_terminal_Terminal.cpp \
+    forkpty.cpp
 
 LOCAL_C_INCLUDES += \
     external/libvterm/include \
@@ -20,9 +21,6 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_STATIC_LIBRARIES := \
     libvterm
-
-LOCAL_CFLAGS := \
-    -Wno-unused-parameter \
 
 LOCAL_MODULE := libjni_terminal
 LOCAL_MODULE_TAGS := optional
